9bba40a11392c9c5acf12165141278dbb68b17ef
reLuna <reLuna@123.com>
2022-11-15 17:58:28 +0800

    2022-11-15,17:58

--- fasta文件的读取.py
+++ fasta文件的读取.py
@@ -1,22 +1,48 @@
 # 开发时间：2022/11/15 14:46
 import pandas as pd
+from Bio.SeqUtils import GC
+from Bio import SeqIO as SI
 pd.set_option('display.max_rows',None)
 pd.set_option('display.max_columns',None)
 pd.set_option('max_colwidth',300)
-f = open('./CYP90D1.fasta')
-a = f.readlines()
+'''BioPyhton方法'''
+sequence = {}
+for i in SI.parse('CYP90D1.fasta','fasta'):
+    # print(i.id)
+    # print(i.seq)
+    print(i.id,GC((i.seq)))
+    sequence[(i.id)] = str(i.seq)
+# print(sequence)
 
-f.close()
 
-sequences = {}
-for i in a:
-    if('>' in i):
-        key_temp = i[1:].strip()
-        print(key_temp)
-    else:
-        sequences[key_temp] = i.strip()
 
-print(sequences)
+
+
+
+
+
+
+
+
+
+
+
+'''传统格式'''
+# f = open('./CYP90D1.fasta')
+# a = f.readlines()
+#
+# f.close()
+
+# sequences = {}
+# for i in a:
+#     # print(i)
+#     if('>' in i):
+#         key_temp = i[10].strip()
+#         print(key_temp)
+#     else:
+#         sequences[key_temp] = i.strip()
+#
+# print(sequences)
 
 # seq= []
 # for i in range(0,len(a)):
