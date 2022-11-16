21b1b2448d15d102ed416fad0f09ac30f29f2b09
reLuna <reLuna@123.com>
2022-11-16 14:11:57 +0800

    2022-11-16,14:11

--- fasta文件的读取.py
+++ fasta文件的读取.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from Bio.SeqUtils import GC
 from Bio import SeqIO as SI
+
 pd.set_option('display.max_rows',None)
 pd.set_option('display.max_columns',None)
 pd.set_option('max_colwidth',300)
@@ -10,10 +11,12 @@ sequence = {}
 for i in SI.parse('CYP90D1.fasta','fasta'):
     # print(i.id)
     # print(i.seq)
-    print('基因ID为:',i.id,'\n','基因序列为：',i.seq,'基因的序列长度为：',len(i.seq),'基因的GC含量为：',GC((i.seq)))
+    # print('基因ID为:',i.id,'\n','基因序列为：',i.seq,'基因的序列长度为：',len(i.seq),'基因的GC含量为：',GC((i.seq)))
     sequence[(i.id)] = str(i.seq)
-# print(sequence)
 
+a = pd.Series(sequence,index=[1,2,3])
+print(a.shape)
+# b= pd.DataFrame(a,index=a.index,columns=[1,2,3])
 
 
 
