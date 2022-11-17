4175189f759091d75d3cb9f7b3d885db84be01c9
reLuna <reLuna@123.com>
2022-11-17 20:00:00 +0800

    2022-11-17,19:59

--- PCA.py
+++ PCA.py
@@ -2,14 +2,14 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-# pd.set_option('display.max_rows',None)
-# pd.set_option('display.max_columns',None)
-# pd.set_option('max_colwidth',300)
+pd.set_option('display.max_rows',None)
+pd.set_option('display.max_columns',None)
+pd.set_option('max_colwidth',300)
 
 
 
-# file_path = './train.csv'
-# df_0 = pd.read_csv(file_path)
+file_path = './train.csv'
+df_0 = pd.read_csv(file_path)
 # # print(df_0.info())
 # # print(df_0.head(10))
 # '''数据整理'''
@@ -30,5 +30,44 @@ from matplotlib import pyplot as plt
 
 df1 = pd.read_csv('titanic_data_pure_PCA.csv',index_col='PassengerId')
 df1.drop(labels='Unnamed: 0',axis=1,inplace=True)
-print(df1.head())
-print(df1.describe())
+
+# print(df1.head())
+# print(df1.describe())
+#normalization 去均值，标准化（去中心化）
+def norm_(x):
+    xmean = np.mean(x,0)
+    std = np.std(x,0)
+    return (x-xmean)/std
+df1_ = norm_(df1)
+print(df1_.describe())
+#V
+#np.linalg.eig()  求矩阵的特征值与特征向量
+# ew,ev = np.linalg.eig(df1_.T.dot(df1_))   #x.T.dot(矩阵内积)x   ==cov(x.T) 上下两个公式得出的特征值有差别
+ew,ev = np.linalg.eig(np.cov(df1_.T))
+  #7个feature对应7个特征值
+#ew特征值的排序
+ew_order = np.argsort(ew)[::-1]
+ew_sort = ew[ew_order]
+ev_sort = ev[:,ew_order]
+print(ew_sort)  #得到的特征值及方差
+print(ev_sort)
+# 取K值，及前K个标准差的和大于全部和95%
+#将取值前7个feature数据降维到K个feature
+#使用前K个数据近似的代表原先的7组数据的结果
+pd.DataFrame(ew_sort).plot(kind = 'bar')
+plt.show()
+
+#ev_sort--->V
+V = ev_sort[:,:2]
+#X_new
+X_new = df1_.dot(V)
+# print(X_new.shape) #(891, 2)   #由原先的7列数据降维到了2列数据
+#scatter
+sc = plt.scatter(X_new.iloc[:,0],X_new.iloc[:,1],s=5,c =df_0.Survived,cmap=plt.cm.coolwarm)
+plt.xlabel('PC0')
+plt.ylabel('PC1')
+plt.colorbar(sc)
+print(V)
+print(df1_.columns)
+
+plt.show()
